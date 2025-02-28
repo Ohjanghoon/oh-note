932536987642cf0b284cffca2cc79f08128719af
Ohjanghoon <ojh4105@naver.com>
2025-02-28 20:47:39 +0900

    [chore] 의존성 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ["ts", "tsx", "mdx"],
 };
 
-export default nextConfig;
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+});
+
+module.exports = withMDX(nextConfig);
