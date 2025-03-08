3face6169075a98613fe526b54d279b4c2c79477
Ohjanghoon <ojh4105@naver.com>
2025-03-09 03:54:09 +0900

    [docs][style] assets 정리 및 게시글 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -10,7 +10,7 @@ function Header() {
       {/* Logo 영역 */}
       <div className="header-logo justify-self-start">
         <Link href="/" className="flex items-center justify-center gap-3">
-          <img src="/logo_oh-note.png" alt="oh-note logo" className="h-8 w-8" />
+          <img src="/assets/logo-oh-note.webp" alt="logo" className="h-8 w-8" />
           <h5 className="font-extrabold tracking-tight">oh-note</h5>
         </Link>
       </div>
