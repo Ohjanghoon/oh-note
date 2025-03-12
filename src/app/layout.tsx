511020330087bd6cfa43846f858963d8273e23e0
Ohjanghoon <ojh4105@naver.com>
2025-03-12 21:08:48 +0900

    [chore][fix][test] geist font 라이브러리 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,14 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 
 // fonts
-import { Geist, Geist_Mono } from "next/font/google";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 
 // styles
 import "@/styles/globals.css";
 
 // components
 import ClientLayout from "@/components/common/ClientLayout";
-
-export const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-
-export const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "oh-note 개발 블로그",
@@ -33,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.className} ${geistMono.className} antialiased`}
+        className={`${GeistSans.className} ${GeistMono.className} antialiased`}
       >
         <ClientLayout>{children}</ClientLayout>
       </body>
