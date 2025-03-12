583bfa58bce625ad6b06cc72c20f0aa7cfe6bea5
Ohjanghoon <ojh4105@naver.com>
2025-03-12 20:59:07 +0900

    [fix][test] font className 속성으로 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.className} ${geistMono.className} antialiased`}
       >
         <ClientLayout>{children}</ClientLayout>
       </body>
