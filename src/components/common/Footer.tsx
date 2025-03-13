01668bfdbb2366248a080ca9e47c3f48ef8492f6
Ohjanghoon <ojh4105@naver.com>
2025-03-13 21:26:06 +0900

    [feat][style] Footer 반응형 작업

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -36,8 +36,8 @@ const LinkList: Link[] = [
 
 function Footer() {
   return (
-    <footer className="root-footer w-full/20 relative z-10 col-span-4 py-10 md:col-span-12">
-      <div className="footer-container flex items-center justify-between rounded-2xl border border-white/10 bg-transparent p-5 shadow-lg ring-1 shadow-black/10 ring-white/20 backdrop-blur-2xl dark:shadow-white/10">
+    <footer className="root-footer relative z-10 col-span-4 mx-auto w-full max-w-screen-md py-10 md:col-span-12 md:px-15">
+      <div className="footer-container dark:bg-background/50 flex flex-col justify-between gap-3 rounded-2xl border border-white/10 bg-transparent p-5 shadow-lg ring-1 shadow-black/10 ring-white/20 backdrop-blur-2xl md:flex-row md:items-center dark:shadow-sm dark:shadow-white/10">
         <div className="footer-content space-y-3">
           <div className="footer-logo flex items-center gap-2">
             <ImageConvert
@@ -55,7 +55,7 @@ function Footer() {
             Copyright &copy; 2025 Janghoon Oh. All rights reserved.
           </p>
         </div>
-        <div className="footer-social flex items-center gap-5">
+        <div className="footer-social flex items-center gap-3">
           {LinkList.map((link) => (
             <SocialLink link={link} key={link.name} />
           ))}
