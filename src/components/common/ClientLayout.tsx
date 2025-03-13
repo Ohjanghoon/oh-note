c1239e574c8c088690e97997c20750c33eed4a6e
Ohjanghoon <ojh4105@naver.com>
2025-03-13 16:25:33 +0900

    [refactor][style] 그라디언트 배경 Footer 재사용성 고려 ClassName props으로 전달 받게 수정

--- src/components/common/ClientLayout.tsx
+++ src/components/common/ClientLayout.tsx
@@ -13,6 +13,7 @@ import BackToTopButton from "@/components/common/BackToTopButton";
 import GradientBackground from "@/components/ui/GradientBackground";
 import Header from "@/components/common/Header";
 import { ThemeProvider } from "@/contexts/ThemeContext";
+import Footer from "./Footer";
 
 const ClientLayout: React.FC<{ children: React.ReactNode }> = ({
   children,
@@ -22,14 +23,16 @@ const ClientLayout: React.FC<{ children: React.ReactNode }> = ({
       <PostsInitializer />
       <ThemeProvider>
         <div className="root-container">
-          <GradientBackground />
+          <GradientBackground styleClassName="w-full top-0 left-0 h-[50vh] opacity-25 blur-3xl" />
           <Header />
           <div className="layout-container">
             <main className="layout-main">{children}</main>
             <aside className="back_to_top">
               <BackToTopButton />
             </aside>
+            <Footer />
           </div>
+          <GradientBackground styleClassName="w-full -bottom-10 left-0 rotate-180 h-60 opacity-20 blur-2xl" />
         </div>
       </ThemeProvider>
     </Provider>
